263a73c8a7135d39a9fe2b719cb92d6ce2536109
Aaron Mednick <aaronjosephm@gmail.com>
2018-07-16 20:53:09 -0400

    First commit for the appointment app.

---
+++ js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.dropdown-trigger').dropdown();
+    $('.materialboxed').materialbox();
+
+    $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+
+
+  }); // end of document ready
+})(jQuery); 
